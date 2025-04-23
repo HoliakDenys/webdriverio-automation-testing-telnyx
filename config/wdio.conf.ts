1aabc9065a3ec5378a1446ebf6848c823d624773
HoliakDenys <denysholiak@gmail.com>
2025-04-23 07:07:22 +0300

    Initial commit

--- config/wdio.conf.ts
+++ config/wdio.conf.ts
@@ -10,7 +10,7 @@ export const config = {
       project: './tsconfig.json'
     }
   },
-  specs: ['../specs/tc9.compare.telnyx.links.spec.ts'],
+  specs: ['../specs/tc10.language.change.spec.ts'],
   baseUrl: process.env.BASE_URL,
   maxInstances: 1,
   logLevel: 'info',
