1dc7e6cf8e698362de8365bd0d60e6b04edb892b
HoliakDenys <denysholiak@gmail.com>
2025-04-23 07:24:36 +0300

    Initial commit

--- config/wdio.conf.ts
+++ config/wdio.conf.ts
@@ -10,7 +10,7 @@ export const config = {
       project: './tsconfig.json'
     }
   },
-  specs: ['../specs/tc20.pricing.navigation.spec.ts'],
+  specs: ['../specs/**.*.ts'],
   baseUrl: process.env.BASE_URL,
   maxInstances: 1,
   logLevel: 'info',
