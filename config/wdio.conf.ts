b0c803356634e0940a5bec8dcd3d2458d5a1f344
HoliakDenys <denysholiak@gmail.com>
2025-04-23 07:15:49 +0300

    Initial commit

--- config/wdio.conf.ts
+++ config/wdio.conf.ts
@@ -10,7 +10,7 @@ export const config = {
       project: './tsconfig.json'
     }
   },
-  specs: ['../specs/tc15.resource.search.spec.ts'],
+  specs: ['../specs/tc16.resource.reset.filtering.spec.ts'],
   baseUrl: process.env.BASE_URL,
   maxInstances: 1,
   logLevel: 'info',
